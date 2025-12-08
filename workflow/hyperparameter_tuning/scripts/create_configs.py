d81e47f08aad69a169d3a76fe663d30920d78ab7
Finlay Clark <finlay.clark@newcastle.ac.uk>
2025-12-08 19:28:51 +0000

    Add hyperparameter tuning scripts

--- workflow/hyperparameter_tuning/scripts/create_configs.py
+++ workflow/hyperparameter_tuning/scripts/create_configs.py
@@ -8,54 +8,38 @@ from loguru import logger
 PLACEHOLDER_SMILES = "C"
 OUTPUT_PATH = Path("configs")
 
-HYPERPARAMETER_OPTIONS = {
-    "ProperTorsion_reg_weight": [0.1, 1.0, 10.0, 100.0],
-    "ImproperTorsion_reg_weight": [0.1, 1.0, 10.0, 100.0],
-}
-
 
 def main() -> None:
     """Create configuration files for hyperparameter tuning."""
 
-    permutations = list(
-        product(
-            HYPERPARAMETER_OPTIONS["ProperTorsion_reg_weight"],
-            HYPERPARAMETER_OPTIONS["ImproperTorsion_reg_weight"],
-        )
+    config = WorkflowSettings(
+        n_iterations=1,
+        parameterisation_settings=ParameterisationSettings(
+            smiles=PLACEHOLDER_SMILES,
+            initial_force_field="openff_unconstrained-2.3.0-rc2.offxml",
+        ),
     )
+    for name in ["ProperTorsions", "ImproperTorsions"]:
+        config.training_settings.parameter_configs[name].regularize = {"k": 1.0}
 
-    # Add unregularised case
-    permutations.append((0.0, 0.0))
+    config_name = "config_metad_std.yaml"
+    output_file = OUTPUT_PATH / config_name
+    OUTPUT_PATH.mkdir(parents=True, exist_ok=True)
+    config.to_yaml(output_file)
+    logger.info(f"Created configuration file: {output_file}")
 
-    # Add case where we don't train propers and impropers at all (inf weight)
-    permutations.append((float("inf"), float("inf")))
+    # Make a new config with updated metadynamics settings
+    config.training_sampling_settings.temperature = "300 K"
+    config.training_sampling_settings.production_sampling_time_per_conformer = "100 ps"
+    config.training_sampling_settings.bias_frequency = "0.4 ps"
+    config.training_sampling_settings.bias_width = 0.017453292519943295
+    config.training_sampling_settings.bias_height = "0.2 kJ * mol**-1"
+    config.training_sampling_settings.bias_factor = 5.0
 
-    for proper_weight, improper_weight in permutations:
-
-        config = WorkflowSettings(
-            n_iterations=1,
-            parameterisation_settings=ParameterisationSettings(
-                smiles=PLACEHOLDER_SMILES,
-                initial_force_field="openff_unconstrained-2.3.0-rc2.offxml",
-            ),
-        )
-
-        for name, weight in [
-            ("ProperTorsions", proper_weight),
-            ("ImproperTorsions", improper_weight),
-        ]:
-            if weight == float("inf"):
-                del config.training_settings.parameter_configs[name]
-            else:
-                config.training_settings.parameter_configs[name].regularize = {
-                    "k": weight
-                }
-
-        config_name = f"config_proper{proper_weight}_improper{improper_weight}.yaml"
-        output_file = OUTPUT_PATH / config_name
-        OUTPUT_PATH.mkdir(parents=True, exist_ok=True)
-        config.to_yaml(output_file)
-        logger.info(f"Created configuration file: {output_file}")
+    config_name = "config_metad_tuned.yaml"
+    output_file = OUTPUT_PATH / config_name
+    config.to_yaml(output_file)
+    logger.info(f"Created configuration file: {output_file}")
 
 
 if __name__ == "__main__":
